59c52959241c25ab6a680dfb861b3c1ec1a558a1
10152132 <hyejoo.jung@kt.com>
2023-02-13 17:36:05 +0900

    Update test code

--- src/test/java/com/example/bank/BankApplicationTests.java
+++ src/test/java/com/example/bank/BankApplicationTests.java
@@ -2,6 +2,7 @@ package com.example.bank;
 
 import com.example.bank.entity.Account;
 import com.example.bank.repository.AccountRepository;
+import com.example.bank.service.AccountService;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,11 +15,19 @@ class BankApplicationTests {
 
     @Autowired
     AccountRepository accountRepository;
+    @Autowired
+    AccountService accountService;
 
     @Test
     void contextLoads() {
         Account acc = new Account("1000","hong",100000,"special","VIP");
         accountRepository.save(acc);
+    }
+
+    @Test
+    void accountInfo() throws Exception{
+        Account acc = accountService.accountInfo("10004");
+        System.out.println(acc);
     }
 
 }
